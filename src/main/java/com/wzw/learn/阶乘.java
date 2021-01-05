d025453f052da821967d3d87b7d05ad81f8f148d
zewen_wang <zewen_wang@kingdee.com>
2021-01-05 09:34:15 +0800

    阶乘-通过循环的方式实现

--- src/main/java/com/wzw/learn/阶乘.java
+++ src/main/java/com/wzw/learn/阶乘.java
@@ -17,8 +17,21 @@ public class 阶乘 {
         return n * method1(--n);
     }
 
+    /**
+     * 通过循环方法实现
+     * @param n
+     * @return
+     */
+    public static int method2(int n){
+        int result = n;
+        while (n > 1) {
+            result = result*(--n);
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
-        System.out.println(method1(4));
+        System.out.println(method1(10));
     }
 
 
