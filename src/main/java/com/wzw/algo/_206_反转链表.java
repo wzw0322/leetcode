9f6ddbf3477d1d10caf0282a452b92a0e9a56606
wzw0322 <469158944@qq.com>
2021-01-13 23:22:48 +0800

    反转链表-递归实现

--- src/main/java/com/wzw/algo/_206_反转链表.java
+++ src/main/java/com/wzw/algo/_206_反转链表.java
@@ -15,7 +15,26 @@ public class _206_反转链表 {
      * @return
      */
     public ListNode reverseList(ListNode head) {
+        //递归终止条件
+        if(head == null || head.next == null){
+            return head;
+        }
+        //找到最后一个节点
+        ListNode newNode = reverseList(head.next);
+        //反转链路关系
+        head.next.next = head;
+        head.next = null;
 
+        //返回的永远是最后的那个节点
+        return newNode;
+    }
+
+    /**
+     * 通过迭代方式实现
+     * @param head
+     * @return
+     */
+    public ListNode reverseList1(ListNode head){
 
         return null;
     }
