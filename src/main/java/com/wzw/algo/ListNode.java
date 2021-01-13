b077b41752134b902589261df93059201dc08bdc
zewen_wang <zewen_wang@kingdee.com>
2021-01-13 10:49:40 +0800

    反转链表定义

---
+++ src/main/java/com/wzw/algo/ListNode.java
@@ -0,0 +1,11 @@
+package com.wzw.algo;
+
+/**
+ * 链表节点
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+
+}
---
+++ src/main/java/com/wzw/algo/_206_反转链表.java
@@ -0,0 +1,22 @@
+package com.wzw.algo;
+
+/**
+ * 反转一个单链表。
+ * 输入: 1->2->3->4->5->NULL
+ * 输出: 5->4->3->2->1->NULL
+ */
+public class _206_反转链表 {
+
+    /**
+     * 通过递归的方式实现
+     * 思路：1.先找到最后一个节点
+     *      2.进行链表关系的反转
+     * @param head
+     * @return
+     */
+    public ListNode reverseList(ListNode head) {
+
+
+        return null;
+    }
+}
